16db36057b70c0107691250a75a17092931b854c
Amanuel Adamu <amanuelhaile18@gmail.com>
2021-08-27 22:57:19 +0300

    fixing betty coding style on task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,37 +6,31 @@
  */
 int main(void)
 {
-	int c, i, k, j;
-
-	for (c = 48; c <= 57; c++)
+	int n, m;
+	
+	for (n = 0; n < 100; n++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (m = 1; m < 100; m++)
 		{
-			for (k = 48; k <= 57; k++)
+			if (n < m)
 			{
-				for (j = 48; j <= 57; j++)
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+				putchar(' ');
+				putchar((m / 10) + '0');
+				putchar((m % 10) + '0');
+				if (n == 98  && m == 99)
 				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
-						if (c + i + k + j == 227 && c == 57)
-						{
-						break;
-						}
-						else
-						{
-						putchar(',');
-						putchar(' ');
-						}
-					}
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
-	putchar('\n');
+	putchar(10);
 	return (0);
 }
