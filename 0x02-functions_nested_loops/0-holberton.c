fd9e9c90cf304fc97abe71032788b68fe85bdb79
Amanuel Adamu <amanuelhaile18@gmail.com>
2021-08-31 13:11:41 +0300

    fixing task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,6 +8,14 @@
  */
 int main(void)
 {
-	_putchar("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('\n');
 	return (0);
 }
