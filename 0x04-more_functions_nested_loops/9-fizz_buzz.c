84a3b508a847e4c157e60f61903978fca138c3c8
Amanuel Adamu <amanuelhaile18@gmail.com>
2021-09-02 19:33:56 +0300

    fixing bugs

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,30 +12,26 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 &&  i % 5 == 0)
 		{
-			if (i % 5 == 0)
-			{
-				printf("FizzBuzz");
-			}
-			else
-			{
-				printf("Fizz");
-			}
+			printf("%s", "FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s", "Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("%s", "Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
-		if (i == 100)
+		if (i != 100)
 		{
-			continue;
+			printf(" ");
 		}
-		printf(" ");
 	}
 	printf("\n");
 	return (0);
