ee61cd51cb81d7a455604a823fdab60d0c6b86de
Amanuel Adamu <amanuelhaile18@gmail.com>
2021-09-06 08:50:40 +0300

    fixing main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Auth: Abinet Tesfu
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
- */
 
 int _putchar(char);
 void print_alphabet(void);
