f53321cff7894489e3d9c3555bc622b1a946f543
Amanuel Adamu <amanuelhaile18@gmail.com>
2021-09-09 12:15:32 +0300

    fixing task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		if (src[i] == '\0')
-			dest[i] = '\0';
-		else
-			dest[i] = src[i];
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 
 	return (dest);
