0b9b4978973a35e0617a5dfc2501a0d086eb8df8
Amanuel Adamu <amanuelhaile18@gmail.com>
2021-09-13 01:23:46 +0300

    ading task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: used to pass the integer
+ * Return : void
+ */
+
+void print_number(int n)
+{
+	char text[20];
+	int i = 0;
+
+	sprintf(text, "%d", n);
+
+	while(text[i] != '\0')
+	{
+		_putchar(text[i]);
+		i++;
+	}
+}
