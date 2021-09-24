21f2441f4e9bd6a1321a8c2686dbfa73ef2f0c78
Amanuel Adamu <amanuelhaile18@gmail.com>
2021-09-24 08:55:09 +0300

    fixing task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,16 +15,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *string;
 	unsigned int i = 0;
 	unsigned int j = 0;
-	unsigned int len;
+	unsigned int len1;
+	unsigned int len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	len = strlen(s2);
-	if (n >= len)
-		n = len;
-	string = malloc(sizeof(s1) + n + 1);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (n >= len2)
+		n = len2;
+	string = malloc(sizeof(char) * (len1 + n + 1));
 	if (string == NULL)
 		return (NULL);
 	while (s1[i] != '\0')
