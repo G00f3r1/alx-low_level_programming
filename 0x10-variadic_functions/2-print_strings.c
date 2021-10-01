5a9540a7c6a14af3949f0d3b2b5b36157a558cc1
Amanuel Adamu <amanuelhaile18@gmail.com>
2021-10-01 04:26:44 +0300

    fixing task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,12 +13,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *s;
 	va_list ap;
 
-
-	if (separator == NULL)
-	{
-		return;
-	}
-
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
