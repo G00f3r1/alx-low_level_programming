78f83f55d62150836d792a93b6e7344f4d959853
Amanuel Adamu <amanuelhaile18@gmail.com>
2021-10-07 23:11:43 +0300

    fixing task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,12 +10,19 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (head)
+	if (head == NULL)
 	{
-		temp = head;
-		head = head->next;
-		free(temp->str);
-		free(temp);
+		free(head);
 	}
-	free(head);
+	else
+	{
+		while (head != NULL)
+		{
+			temp = head;
+			head = head->next;
+			free(temp->str);
+			free(temp);
+		}
+		free(head);
+	}
 }
