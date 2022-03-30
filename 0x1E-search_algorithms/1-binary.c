80c1a7d4af8266b2aaa60617bde5b77ef7cfde22
Amanuel Adamu <amanueladamu18@gmail.com>
2022-03-30 03:08:47 +0000

    fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -44,7 +44,7 @@ int binary_search(int *array, size_t size, int value)
 		if (array[middle] < value)
 			left = middle + 1;
 		else if (array[middle] > value)
-			right = middle - 1;
+			right = middle;
 		else
 			return (middle);
 		print_array(array + left, right + 1 - left);
